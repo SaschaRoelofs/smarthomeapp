f5ca4a53b3cd7197bb6477866a87ba0e64adf429
SMartHome <sascha-roelofs.de>
2020-03-04 12:57:37 +0100

    DashboardScreen ScrollView With Real Data

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,48 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { StyleSheet, Text, View, Button, FlatList, SafeAreaView, ScrollView } from 'react-native'
 import Firebase from '../components/Firebase';
 
 const DashboardScreen = ({ navigation }) => {
 
-
     const [items, setItems] = useState([]);
 
-    function Item({ title }) {
-        return (
-            <View style={styles.item}>
-                <Text style={styles.title}>{title}</Text>
-            </View>
-        );
-    }
-
-    handleAction = () => {
-        //const currentUser = Firebase.auth().currentUser
-        //console.log(currentUser.email);
+    useEffect(() => {
+        // const currentUser = Firebase.auth().currentUser
+        // console.log(currentUser.email);
         axios.get('http://5.181.50.205:4000/data-handler?users=sascha@web.de') //+ currentUser.email)
             .then((response) => {
-                setItems([
-                    ...items,
-                    {
-                        id: response.data.devicekey,
-                        value: response.data.state
-                    }
-                ]);
-
+                setItems(response.data)
             })
             .catch((error) => {
                 console.error(error);
             })
-    }
-
+    })
 
     return (
         <View>
             <Button title="Garagentor" onPress={() => handleAction()} />
             <ScrollView>
                 {items.map(item => (
-                    <View key={item.id}>
-                        <Text style={styles.item}>{item.value}</Text>
+                    <View key={item.devicekey}>
+                        <Text style={styles.item}>{item.state}</Text>
                     </View>
                 ))}
             </ScrollView>

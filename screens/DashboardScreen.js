0c3b02129e64d7710259a4a5d80eb8686676ee95
SMartHome <sascha-roelofs.de>
2020-03-04 18:10:07 +0100

    Auslog Button

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -9,34 +9,46 @@ const DashboardScreen = ({ navigation }) => {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        const currentUser = Firebase.auth().currentUser
-        axios.get('http://5.181.50.205:4000/data-handler?users=' + currentUser.email)
-            .then((response) => {
-                setItems(response.data)
-            })
-            .catch((error) => {
-                console.error(error);
-            })
-    } )
+        Firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                const currentUser = Firebase.auth().currentUser
+                axios.get('http://5.181.50.205:4000/data-handler?users=' + currentUser.email)
+                    .then((response) => {
+                        setItems(response.data)
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    })
+            } else {
+                console.log("Fehler Dashboard")
+            }
+        });
 
-     const onPressHandler = (devicekey, state) =>  {
+    })
+
+    const onPressHandler = (devicekey, state) => {
         axios.patch("http://5.181.50.205:4000/data-handler", {
             "devicekey": devicekey,
             "state": (state == 0) ? 1 : 0
         })
-        .then((response) => {
-               console.log(response.data);
-        });
-     }
-     
+            .then((response) => {
+                console.log(response.data);
+            });
+    }
+
+    const signOutHandler = () => {
+        Firebase.auth().signOut()
+        navigation.navigate('LoginScreen')
+    }
 
     return (
         <View>
+            <Button title="Ausloggen" onPress={() => signOutHandler()} />
             <ScrollView>
                 {items.map(item => (
                     <View key={item.devicekey}>
                         <TouchableNativeFeedback onPress={() => onPressHandler(item.devicekey, item.state)}>
-                        <Text style={styles.item}>{item.state}</Text>
+                            <Text style={styles.item}>{item.state}</Text>
                         </TouchableNativeFeedback>
                     </View>
                 ))}

d0ce3cfbc7d8add885cd7cfc3528aab3900240ab
SMartHome <sascha-roelofs.de>
2020-03-04 11:02:26 +0100

    DashboardScreen ScrollView With ExampleData

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,24 +1,69 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
-import { Card, ListItem, Icon } from 'react-native-elements'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from 'react'
+import axios from 'axios';
+import { StyleSheet, Text, View, Button, FlatList, SafeAreaView, ScrollView } from 'react-native'
+import Firebase from '../components/Firebase';
 
 const DashboardScreen = ({ navigation }) => {
 
-    handleAction = () => {
-        console.log("handleAction");
+
+    const [items, setItems] = useState([]);
+
+    function Item({ title }) {
+        return (
+            <View style={styles.item}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+        );
     }
+
+    handleAction = () => {
+        //const currentUser = Firebase.auth().currentUser
+        //console.log(currentUser.email);
+        axios.get('http://5.181.50.205:4000/data-handler?users=sascha@web.de') //+ currentUser.email)
+            .then((response) => {
+                setItems([
+                    ...items,
+                    {
+                        id: response.data.devicekey,
+                        value: response.data.state
+                    }
+                ]);
+
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
 
     return (
         <View>
-            {/* <Button title="LoginScreen" onPress={() => navigation.navigate('LoginScreen')} /> */}
             <Button title="Garagentor" onPress={() => handleAction()} />
-
+            <ScrollView>
+                {items.map(item => (
+                    <View key={item.id}>
+                        <Text style={styles.item}>{item.value}</Text>
+                    </View>
+                ))}
+            </ScrollView>
         </View>
     )
 }
 
 export default DashboardScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //marginTop: 10,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+});

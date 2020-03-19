093da1a85103f4347f6bd519e3e25262bd4eb408
SMartHome <78116@studmail.htw-aalen.de>
2020-03-19 12:26:19 +0100

    Richtiges google-service.json file

--- android/app/src/main/java/com/smart/MainApplication.java
+++ android/app/src/main/java/com/smart/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.smart;
 
 import android.app.Application;
 import android.content.Context;
--- ios/SMartTests/SMartTests.m
+++ ios/SMartTests/SMartTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface SMartTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation SMartTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
